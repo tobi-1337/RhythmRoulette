3ce5953f24217df202288ce84d14d4b0a085c65d
tobi-1337 <tobiasbjorklund1@gmail.com>
2024-03-27 11:49:50 +0100

    GET-TOP-ARTISTS
    FUNKAR

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, url_for, session, redirect
+from flask import Flask, render_template, url_for, session, redirect, request
 import os
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth 
 from spotipy.cache_handler import FlaskSessionCacheHandler
-from config import client_id, client_secret, redirect_uri, scope
+from config import client_id, client_secret, redirect_uri
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hello"
-
+scope = 'user-top-read'
 cache_handler = FlaskSessionCacheHandler(session)
 sp_oauth = SpotifyOAuth(
     client_id=client_id, 
@@ -26,9 +26,32 @@ sp = Spotify(auth_manager=sp_oauth)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for('get_top_artists'))
 
+@app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_top_artists'))
 
+@app.route('/top-artists')
+def get_top_artists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
 
+    top_artists = sp.current_user_top_artists()
+    artists = top_artists['items']
+    for artist in artists:
+        print(artist['name'])
+
+    return render_template('top-artists.html', artists=artists)
+    
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
