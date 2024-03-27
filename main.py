8bd1f0775d745f94e28599e12f179b4db9b91183
tobi-1337 <tobiasbjorklund1@gmail.com>
2024-03-27 10:38:40 +0100

    provar om config.py syns

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("Hello world!")
-print('whoho!!')
-print('12345679')
+from flask import Flask, render_template, url_for, session, redirect
+import os
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth 
+from spotipy.cache_handler import FlaskSessionCacheHandler
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "hello"
+
