3b22450080f29337ce0b38eb02d409ea659eda2b
tobi-1337 <tobiasbjorklund1@gmail.com>
2024-03-27 10:57:19 +0100

    skapade första HTML
    och kopplade med flask

--- main.py
+++ main.py
@@ -3,7 +3,32 @@ import os
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth 
 from spotipy.cache_handler import FlaskSessionCacheHandler
+from config import client_id, client_secret, redirect_uri, scope
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "hello"
 
+cache_handler = FlaskSessionCacheHandler(session)
+sp_oauth = SpotifyOAuth(
+    client_id=client_id, 
+    client_secret=client_secret, 
+    redirect_uri=redirect_uri, 
+    cache_handler=cache_handler,
+    scope=scope,
+    show_dialog = True
+)
+
+sp = Spotify(auth_manager=sp_oauth)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
