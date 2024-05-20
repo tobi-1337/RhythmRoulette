4f837b265cbdf98d22d1f4886e80e3e4cc355e44
tobi-1337 <156711407+tobi-1337@users.noreply.github.com>
2024-05-20 10:24:01 +0200

    now profile page doesnt crash

--- main.py
+++ main.py
@@ -199,17 +199,14 @@ def user_profile(username):
     if not search_name:
         return render_template('index.html')
     user = user_info(username)
+    print(user['id'])
     username = user['id']
     current_user = get_user_info('username')
     display_name = user['display_name']
     user_image_url = user['images'][0]['url'] if user['images'] else None
-
-    user_bio = db.save_user_bio(username)
-    if user_bio:
-        return render_template('profile_page.html', username=username,current_user=current_user,display_name=display_name,user_image_url=user_image_url,user_bio=user_bio)
-    else:
-        not_in_bio = "Det finns ingen biografi för denna användare ännu"
-    return render_template('profile_page.html',username=username,current_user=current_user,display_name=display_name,user_image_url=user_image_url,user_bio=not_in_bio)
+    user_bio = db.get_user_bio(username)
+    print(user_bio)
+    return render_template('profile_page.html', username=username, display_name=display_name, user_image_url=user_image_url,current_user=current_user, user_bio = user_bio)
 
     
 @app.route('/profile-settings')
